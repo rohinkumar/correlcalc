86816ce853d9698b2328e321aa3e3682e4ba523a
Rohin Kumar <yrohinkumar@gmail.com>
2017-10-16 21:16:18 +0530

    develop branch started...need to experiment around param

--- correlcalc/param.py
+++ correlcalc/param.py
@@ -1,4 +1,11 @@
 # Set global parameters here related to Cosmology (more to be added)
 # Om= matter density Ol=dark energy density at the present epoch
-Om = 0.3
-Ol = 0.7
+# Planck 2015
+Om = 0.3121
+Ol = 0.6879
+# WMAP9
+# Om = 0.279
+# Ol = 0.721
+# Default
+# Om = 0.3
+# Ol = 0.7
